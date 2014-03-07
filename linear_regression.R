f096d80c73e2299c1ec81a1ea051db61f2a1ccc0
cmlakhan <chirag.lakhani@gmail.com>
2014-03-06 23:31:44 -0500

    multiple linear regression model with interaction terms included

--- linear_regression.R
+++ linear_regression.R
@@ -16,8 +16,33 @@ predict(lm.fit, data.frame(lstat=(c(5,10,15))),interval="confidence")
 plot(Boston$lstat,Boston$medv)
 abline(lm.fit)
 
+
 par(mfrow=c(2,2))
 plot(lm.fit)
 
+par(mfrow=c(1,1))
 plot(predict(lm.fit), residuals(lm.fit))
 plot(predict(lm.fit), rstudent(lm.fit))
+
+
+
+lm.fit=lm(medv~.,data=Boston)
+summary(lm.fit)
+
+
+
+
+
+par(mfrow=c(3,3))
+plot(Boston$crim,Boston$medv)
+plot(Boston$zn,Boston$medv)
+plot(Boston$nox,Boston$medv)
+plot(Boston$crim,Boston$medv)
+plot(Boston$nox,Boston$medv)
+plot(Boston$dis,Boston$medv)
+plot(Boston$rad,Boston$medv)
+plot(Boston$ptratio,Boston$medv)
+plot(Boston$black,Boston$medv)
+
+par(mfrow=c(2,2))
+plot(lm.fit)
