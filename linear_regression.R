39db02e059317f3adf9d2d9de7637b200106581b
cmlakhan <chirag.lakhani@gmail.com>
2014-03-06 22:21:36 -0500

    added some plots to 1-D regression

--- linear_regression.R
+++ linear_regression.R
@@ -8,4 +8,16 @@ names(Boston)
 
 lm.fit=lm(medv~lstat,data=Boston)
 
+lm.fit
 summary(lm.fit)
+confint(lm.fit)
+predict(lm.fit, data.frame(lstat=(c(5,10,15))),interval="confidence")
+
+plot(Boston$lstat,Boston$medv)
+abline(lm.fit)
+
+par(mfrow=c(2,2))
+plot(lm.fit)
+
+plot(predict(lm.fit), residuals(lm.fit))
+plot(predict(lm.fit), rstudent(lm.fit))
