78eb01429f2290410765ee080523ba7569c74821
Chirag Lakhani <chirag@Chirags-MacBook-Air.local>
2014-06-07 18:57:09 -0400

    Did validation set, LOOCV, and k-fold CV to calculate test error on prostate cancer dataset.

--- resampling_methods_ch5.R
+++ resampling_methods_ch5.R
@@ -8,5 +8,42 @@ colnames(prostate_cancer)[5] <- "rectalExamResult"
 colnames(prostate_cancer)[6] <- "capsularInvolvement"
 colnames(prostate_cancer)[7] <- "antigenValue"
 colnames(prostate_cancer)[8] <- "tumorVolume"
-colnames(prostate_cancer)[1] <- "gleasonScore"
+colnames(prostate_cancer)[9] <- "gleasonScore"
 
+prostate_cancer$race <- as.factor(prostate_cancer$race)
+prostate_cancer$rectalExamResult <- as.factor(prostate_cancer$rectalExamResult)
+
+
+attach(prostate_cancer)
+
+
+##Validation Set
+train=sample(380,200)
+glm.fit=glm(tumor~age+race+rectalExamResult+capsularInvolvement+antigenValue+gleasonScore,data=prostate_cancer,family = binomial,subset=train)
+mean((tumor-predict(glm.fit,prostate_cancer, type="response"))[-train]^2)
+
+##LOOCV
+library(boot)
+glm.fit=glm(tumor~age+race+rectalExamResult+capsularInvolvement+antigenValue+gleasonScore,data=prostate_cancer,family = binomial)
+coef(glm.fit)
+kfCV <- cv.glm(data=prostate_cancer, glmfit=glm.fit)
+kfCV$delta
+
+i <-  2
+#Polynomial Logistic Regression LOOCV
+cv.error=rep(0,5)
+for (i in 1:5){
+  glm.fit=glm(tumor~age+race+rectalExamResult+capsularInvolvement+poly(antigenValue,i)+poly(gleasonScore,i),data=prostate_cancer,family = binomial)
+  cv.error[i]=cv.glm(data=prostate_cancer, glmfit=glm.fit)$delta[1]
+}
+
+cv.error
+
+
+#Polynomial Logistic Regresion CV K=5
+cv.error=rep(0,4)
+for (i in 1:4){
+  glm.fit=glm(tumor~poly(age,i)+race+rectalExamResult+capsularInvolvement+antigenValue+gleasonScore,data=prostate_cancer,family = binomial)
+
+  cv.error[i]=cv.glm(data=prostate_cancer, glmfit=glm.fit,K=5)$delta[1]
+}
